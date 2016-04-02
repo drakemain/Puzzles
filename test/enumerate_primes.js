3cc48e1d62b55a5fcd2bd3a92fc77b7d00140402
drakemain <demain1990@gmail.com>
2016-04-01 21:43:26 -0700

    more descriptive test names

--- test/enumerate_primes.js
+++ test/enumerate_primes.js
@@ -3,7 +3,7 @@ var assert = chai.assert;
 
 var enumeratePrimes = require('./../src/enumerate_primes');
 
-describe('Array', function() {
+describe('Enumerate Primes: ', function() {
   it('should have all primes preceeding given integer', function() {
     var input = 18;
     var expected = [2, 3, 5, 7, 11, 13, 17];
--- test/remove_duplicates.js
+++ test/remove_duplicates.js
@@ -3,7 +3,7 @@ var assert = chai.assert;
 
 var removeDuplicates = require('./../src/remove_duplicates');
 
-describe('Array', function(){
+describe('Remove Duplicates: ', function(){
   it('Should remove duplicates', function() {
     var arr = [1, 2, 3, 5, 5, 7, 9, 9, 11, 11, 11, 13];
     var expected = [1, 2, 3, 5, 7, 9, 11, 13, 0, 0, 0, 0];
