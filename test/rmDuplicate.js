946b58823501380aed799e73ee5610d00bd190d0
drakemain <demain1990@gmail.com>
2016-04-01 19:34:30 -0700

    added empty array test

--- test/rmDuplicate.js
+++ test/rmDuplicate.js
@@ -1,18 +1,25 @@
 var chai = require('chai');
 var assert = chai.assert;
 
-rmduplicates = require('./../src/rmDuplicate');
-
-var arr = [1, 2, 3, 5, 5, 7, 9, 9, 11, 11, 11, 13];
-var expected = [1, 2, 3, 5, 7, 9, 11, 13, 0, 0, 0, 0];
+var rmduplicates = require('./../src/rmDuplicate');
 
 describe('Array', function(){
-  it('Should equal expected', function() {
+  it('Should remove duplicates', function() {
+    var arr = [1, 2, 3, 5, 5, 7, 9, 9, 11, 11, 11, 13];
+    var expected = [1, 2, 3, 5, 7, 9, 11, 13, 0, 0, 0, 0];
+
     rmduplicates(arr);
 
-    console.log(arr);
-    console.log(expected);
+    assert.deepEqual(arr, expected);
+  });
+
+  it('Should handle empty arrays', function() {
+    var arr = [];
+    var expected = [];
+
+    rmduplicates(arr);
 
     assert.deepEqual(arr, expected);
+
   });
 });
