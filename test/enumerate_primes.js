2e1b110fc0234107aaf2aed834f94b09fc8b9212
drakemain <demain1990@gmail.com>
2016-04-01 21:46:37 -0700

    housecleaning

--- test/enumerate_primes.js
+++ test/enumerate_primes.js
@@ -3,12 +3,8 @@ var assert = chai.assert;
 
 var enumeratePrimes = require('./../src/enumerate_primes');
 
-describe('Enumerate Primes: ', function() {
+describe('Enumerate Primes', function() {
   it('should have all primes preceeding given integer', function() {
-    var input = 18;
-    var expected = [2, 3, 5, 7, 11, 13, 17];
-    var result = enumeratePrimes(input);
-
-    assert.deepEqual(result, expected);
+    assert.deepEqual(enumeratePrimes(18), [2, 3, 5, 7, 11, 13, 17]);
   });
 });
