65d54c9e46335d43739dd631e8b242f743c0a36e
drakemain <demain1990@gmail.com>
2016-04-04 16:21:18 -0700

    sinusoidal string

--- src/sinusoidal_string.js
+++ src/sinusoidal_string.js
@@ -1,3 +1,17 @@
 module.exports = function(input) {
-	return "test";
+	var output = '';
+
+	for (var i = 1; i < input.length; i += 4) {
+		output += input.substr(i, 1);
+	}
+
+	for (var i = 0; i < input.length; i += 2) {
+		output += input.substr(i,1);
+	}
+
+	for (var i = 3; i < input.length; i += 4) {
+		output += input.substr(i,1);
+	}
+
+	return output;
 }
