16dbc77b3b21603a82e9b6437c34a1b7358b1d3e
drakemain <demain1990@gmail.com>
2016-05-09 15:59:13 -0700

    tabs to spaces

--- src/even_odd_merge.js
+++ src/even_odd_merge.js
@@ -1,32 +1,32 @@
 var List = require('./linked_list').List;
 
 module.exports = function(head) {
-	var lookingAt = head,
-			evenTail,
-			oddHead,
-			oddTail;
+  var lookingAt = head,
+      evenTail,
+      oddHead,
+      oddTail;
 
-	while (lookingAt !== null) {
-		console.log(lookingAt.value);
-		if (lookingAt.value % 2 === 0) {
-			if (evenTail) {
-				evenTail.next = lookingAt;
-				oddTail.next = lookingAt.next;
-			}
+  while (lookingAt !== null) {
+    console.log(lookingAt.value);
+    if (lookingAt.value % 2 === 0) {
+      if (evenTail) {
+        evenTail.next = lookingAt;
+        oddTail.next = lookingAt.next;
+      }
 
-			evenTail = lookingAt;
+      evenTail = lookingAt;
 
-		} else {
-			if (oddHead) {
-				oddTail.next = lookingAt;
-				oddTail = lookingAt;
-			} else {
-				oddHead = lookingAt;
-				oddTail = lookingAt;
-			}
-		}
-		lookingAt = lookingAt.next;
-	}
+    } else {
+      if (oddHead) {
+        oddTail.next = lookingAt;
+        oddTail = lookingAt;
+      } else {
+        oddHead = lookingAt;
+        oddTail = lookingAt;
+      }
+    }
+    lookingAt = lookingAt.next;
+  }
 
-	evenTail.next = oddHead;
+  evenTail.next = oddHead;
 }
--- test/even_odd_merge.js
+++ test/even_odd_merge.js
@@ -5,14 +5,14 @@ var List = require('./../src/linked_list').List;
 var evenOddArrays = require('./../src/even_odd_merge');
 
 describe("Even Odd Merge", function() {
-	it("should arrange nodes to start with even and end with odds", function() {
-		var thisList = List.fromArray([0, 1, 2, 3, 4]),
-				expectedList = List.fromArray([0, 2, 4, 1, 3]);
+  it("should arrange nodes to start with even and end with odds", function() {
+    var thisList = List.fromArray([0, 1, 2, 3, 4]),
+        expectedList = List.fromArray([0, 2, 4, 1, 3]);
 
-		evenOddArrays(thisList);
+    evenOddArrays(thisList);
 
-		console.log(thisList.toString());
+    console.log(thisList.toString());
 
-		assert(thisList.equals(expectedList), 'fail');
-	});
+    assert(thisList.equals(expectedList), 'fail');
+  });
 });
