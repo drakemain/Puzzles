e94ec033b23394994b439b2af4e7565f9b16894e
drakemain <demain1990@gmail.com>
2016-05-27 19:16:14 -0700

    insert method for binary tree

--- javascript/src/binary_tree.js
+++ javascript/src/binary_tree.js
@@ -6,6 +6,34 @@ function BinaryTree(value, left, right) {
   this.right = right || null;
 }
 
+BinaryTree.prototype.insert = function(value) {
+  var lookingAt = this;
+
+  while (true) {
+    if (value === lookingAt.value) {
+      return;
+    } else if (value < lookingAt.value) {
+
+      if (lookingAt.left === null) {
+        lookingAt.left = new BinaryTree(value);
+        return;
+      } else {
+        lookingAt = lookingAt.left;
+      }
+
+    } else if (value > lookingAt.value) {
+     
+      if(lookingAt.right === null) {
+        lookingAt.right = new BinaryTree(value);
+        return;
+      } else {
+        lookingAt = lookingAt.right;
+      }
+
+    }
+  }
+};
+
 BinaryTree.prototype.contains = function(value) {
   var lookingAt = this;
 
--- javascript/test/binary_tree.js
+++ javascript/test/binary_tree.js
@@ -16,4 +16,27 @@ describe("Binary Tree", function() {
     assert(!tree.contains(11), "found 11");
     assert(!tree.contains(16), "found 16");
   });
+
+  it("should be able to insert values into the tree", function() {
+    var tree = new BinaryTree(10, new BinaryTree(5), new BinaryTree(15));
+
+    tree.insert(3);
+    tree.insert(8);
+    tree.insert(12);
+    tree.insert(17);
+
+    tree.insert(2);
+    tree.insert(1);
+    tree.insert(0);
+
+    console.log(tree);
+
+    assert(tree.contains(3), "didn't find 3");
+    assert(tree.contains(8), "didn't find 8");
+    assert(tree.contains(12), "didn't find 12");
+    assert(tree.contains(17), "didn't find 17");
+    assert(tree.contains(2), "didn't find 2");
+    assert(tree.contains(1), "didn't find 1");
+    assert(tree.contains(0), "didn't find 0");
+  });
 });
