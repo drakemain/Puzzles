c2e25306b37cea70ee1951d6cf660c4a2a3b3f7b
drakemain <demain1990@gmail.com>
2016-05-28 17:15:15 -0700

    map to specified values

--- javascript/src/binary_tree.js
+++ javascript/src/binary_tree.js
@@ -13,7 +13,6 @@ BinaryTree.prototype.insert = function(value) {
     if (value === lookingAt.value) {
       return;
     } else if (value < lookingAt.value) {
-
       if (lookingAt.left === null) {
         lookingAt.left = new BinaryTree(value);
         return;
@@ -22,14 +21,12 @@ BinaryTree.prototype.insert = function(value) {
       }
 
     } else if (value > lookingAt.value) {
-     
       if(lookingAt.right === null) {
         lookingAt.right = new BinaryTree(value);
         return;
       } else {
         lookingAt = lookingAt.right;
       }
-
     }
   }
 };
@@ -49,3 +46,25 @@ BinaryTree.prototype.contains = function(value) {
 
   return false;
 };
+
+BinaryTree.prototype.mapTo = function(value) {
+  var lookingAt = this
+  , map = "";
+
+  while (lookingAt !== null) {
+    map += lookingAt.value;
+
+    if (value === lookingAt.value) {
+      break;
+    } else if (value < lookingAt.value) {
+      lookingAt = lookingAt.left;
+    } else if (value > lookingAt.value) {
+      lookingAt = lookingAt.right;
+    }
+
+    map += " → ";
+  }
+
+  return map;
+};
+
