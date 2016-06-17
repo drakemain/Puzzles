5bb3ee234a590e6f13c99602198794fec591e664
drakemain <demain1990@gmail.com>
2016-06-17 01:26:52 -0700

    use -g for debugging

--- c++/build.sh
+++ c++/build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-clang++ -std=c++11 -Wall -Wextra -Werror ./src/linked-list.cc -o out/linked-list.bin
+clang++ -std=c++11 -Wall -Wextra -Werror -g ./src/linked-list.cc -o out/linked-list.bin
