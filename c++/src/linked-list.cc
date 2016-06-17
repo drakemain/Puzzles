dfa6f91e4b60de38d10f50706c4047805d1ba270
drakemain <demain1990@gmail.com>
2016-06-16 23:33:55 -0700

    build a full list
    Previously was just building a single node. Now building a list.

--- c++/src/linked-list.cc
+++ c++/src/linked-list.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 class Node {
 private:
@@ -7,6 +8,7 @@ private:
   Node* next;
 
 public:
+  //constructors
   Node() {
     next = nullptr;
   };
@@ -16,14 +18,28 @@ public:
     this->next = nullptr;
   }
 
+  Node(int value, Node *next) {
+    this->value = value;
+    this->next = next;
+  }
+
+  // destructor
+  ~Node() {
+    if (this->next) {
+      delete this->next;
+    }
+  }
+  
+
   // getters
   int getValue() {
     return this->value;
   }
 };
 
-int main() {
-  Node test(1);
 
-  std::cout << test.getValue() << '\n';
+int main() {
+  Node heapList = Node(0, new Node(1, new Node(2, new Node(3, new Node(4, new Node(5, nullptr))))));
+
+  std::cout << heapList.toString() << std::endl;
 }
