cd7082292f5ae5941fc4384a6d5542b848554af9
drakemain <demain1990@gmail.com>
2016-09-04 03:30:58 -0700

    add method for deleting nodes by value

--- c++/src/binary-tree.cc
+++ c++/src/binary-tree.cc
@@ -4,13 +4,12 @@
 #include <cassert>
 
 template <typename T>
-class Node {
-private:
+struct Node {
   T value;
   Node* left = nullptr;
   Node* right = nullptr;
 
-public:
+
   Node(T value) : value(value) {}
 
   Node(T value, Node* left, Node* right)
@@ -20,28 +19,6 @@ public:
   ~Node() {
     delete this->left;
     delete this->right;
-    std::cout<<"NODE DESTR: " << this->value << std::endl;
-  }
-
-
-  T getValue() {
-    return this->value;
-  }
-
-  Node* getLeft() {
-    return this->left;
-  }
-
-  Node* getRight() {
-    return this->right;
-  }
-
-  void setLeft(Node* node) {
-    this->left = node;
-  }
-
-  void setRight(Node* node) {
-    this->right = node;
   }
 };
 
@@ -51,12 +28,13 @@ private:
   Node<T>* root;
 
 public:
+  Tree() {}
+
   Tree(T value) : root(new Node<T>(value)) {}
 
 
   ~Tree() {
     delete this->root;
-    std::cout<<"TREE DESTR"<<std::endl;
   }
 
 
@@ -69,18 +47,18 @@ public:
         break;
       }
 
-      os << lookingAt->getValue();
+      os << lookingAt->value;
 
-      if (lookingAt->getValue() == value) {
+      if (lookingAt->value == value) {
         break; 
       }
 
-      if (value > lookingAt->getValue()) {
+      if (value > lookingAt->value) {
         os << " (R)";
-        lookingAt = lookingAt->getRight();
-      } else if (value < lookingAt->getValue()) {
+        lookingAt = lookingAt->right;
+      } else if (value < lookingAt->value) {
         os << " (L)";
-        lookingAt = lookingAt->getLeft();
+        lookingAt = lookingAt->left;
       }
 
       os << "→ ";
@@ -88,64 +66,174 @@ public:
   }
 
   void insert(T value) {
+    if (this->root == nullptr) {this->root = new Node<T>(value);return;}
+
     Node<T>* lookingAt = this->root;
 
     while (true) {
-      if (value > lookingAt->getValue()) {
+      if (value > lookingAt->value) {
         // look right
-        if (lookingAt->getRight() == nullptr) {
-          lookingAt->setRight(new Node<T>(value));
+        if (lookingAt->right == nullptr) {
+          lookingAt->right = new Node<T>(value);
           return;
         } else {
-          lookingAt = lookingAt->getRight();
+          lookingAt = lookingAt->right;
         }
 
-      } else if (value < lookingAt->getValue()) {
+      } else if (value < lookingAt->value) {
         // look left
-        if (lookingAt->getLeft() == nullptr) {
-          lookingAt->setLeft(new Node<T>(value));
+        if (lookingAt->left == nullptr) {
+          lookingAt->left = new Node<T>(value);
           return;
         } else {
-          lookingAt = lookingAt->getLeft();
+          lookingAt = lookingAt->left;
         }
       
-      } else if (value == lookingAt->getValue()) {
+      } else if (value == lookingAt->value) {
         return;
       }
     }
   }
 
-  // void deleteByValue(T value) {
-  //   Node* lookingAt = this->root;
+  void deleteByValue(T value) {
+    if (this->root == nullptr) {return;}
 
-  //   while (lookingAt-> != )
-  // }
+    Node<T>* lookingAt = this->root;
+    Node<T>** previousPtr = nullptr;
+
+    while (lookingAt->value != value) {
+
+      if (value > lookingAt->value) {
+        previousPtr = &lookingAt->right;
+        lookingAt = lookingAt->right;
+      } else if (value < lookingAt->value) {
+        previousPtr = &lookingAt->left;
+        lookingAt = lookingAt->left;
+      }
+
+      if (lookingAt == nullptr) {return;}
+    }
+
+    this->detatchNode(lookingAt, previousPtr);
+
+    delete lookingAt;
+  }
+
+private:
+  T getNextLargestValue(Node<T>* startingNode) {
+    Node<T>* lookingAt = startingNode;
+
+    lookingAt = lookingAt->right;
+
+    while (lookingAt->left != nullptr) {
+      lookingAt = lookingAt->left;
+    }
+
+    return lookingAt->value;
+  }
+
+  void detatchNode(Node<T>* nodeToDelete, Node<T>** ptrToNode) {
+    if (nodeToDelete->right == nullptr && nodeToDelete->left == nullptr) {
+      if (ptrToNode == nullptr) {this->root = nullptr;} 
+      else {*ptrToNode = nullptr;}      
+
+    } else if (nodeToDelete->right != nullptr && nodeToDelete->left != nullptr) {
+      T swapValue = this->getNextLargestValue(nodeToDelete);
+      this->deleteByValue(swapValue);
+
+      if(ptrToNode == nullptr) {
+        Node<T>* newRoot = new Node<T>(swapValue, nodeToDelete->left, nodeToDelete->right);
+        this->root = newRoot;   
+      } else {
+        *ptrToNode = new Node<T>(swapValue, nodeToDelete->left, nodeToDelete->right);        
+      }
+
+      nodeToDelete->left = nullptr;
+      nodeToDelete->right = nullptr;
+      
+    } else if (nodeToDelete->right) {
+      if (ptrToNode == nullptr) {this->root = nodeToDelete->right;} 
+      else {*ptrToNode = nodeToDelete->right;}
+      
+      nodeToDelete->right = nullptr;
+
+    } else if (nodeToDelete->left) {
+      if (ptrToNode == nullptr) {this->root = nodeToDelete->left;} 
+      else {*ptrToNode = nodeToDelete->left;}
+      
+      nodeToDelete->left = nullptr;
+    }
+  }
 };
 
+template <typename T>
+void testPathTo(Tree<T> &tree, T value, std::string expectedPath);
+
 int main() {
-  Tree<int> tree = Tree<int>(1); tree.insert(0); tree.insert(2); tree.insert(5); tree.insert(4);
+  Tree<int> tree = Tree<int>(8); tree.insert(3); tree.insert(10); tree.insert(1); tree.insert(6);
+    tree.insert(6); tree.insert(14); tree.insert(4); tree.insert(7); tree.insert(13);
 
-  std::ostringstream pathString;
+  Tree<int> tree1 = Tree<int>(8); tree1.insert(3); tree1.insert(10); tree1.insert(1); tree1.insert(6);
+    tree1.insert(6); tree1.insert(14); tree1.insert(4); tree1.insert(7); tree1.insert(13);
 
-  tree.pathTo(4, pathString);
-  assert(pathString.str() == "1 (R)→ 2 (R)→ 5 (L)→ 4");
-  pathString.str("");
+  Tree<int> linearTreeRight = Tree<int>(8); linearTreeRight.insert(10); linearTreeRight.insert(14);
+    linearTreeRight.insert(15);
 
-  tree.pathTo(6, pathString);
-  assert(pathString.str() == "1 (R)→ 2 (R)→ 5 (R)→ ∅");
-  pathString.str("");
+  Tree<int> singleNodeTree = Tree<int>(8);
 
-  tree.pathTo(0, pathString);
-  assert(pathString.str() == "1 (L)→ 0");
-  pathString.str("");
+  // test insert working as expected; path to leaf nodes
+  testPathTo(tree, 1, "8 (L)→ 3 (L)→ 1");
+  testPathTo(tree, 4, "8 (L)→ 3 (R)→ 6 (L)→ 4");
+  testPathTo(tree, 7, "8 (L)→ 3 (R)→ 6 (R)→ 7");
+  testPathTo(tree, 13, "8 (R)→ 10 (R)→ 14 (L)→ 13");
+
+  // test path to nodes that don't exist
+  testPathTo(tree, 15, "8 (R)→ 10 (R)→ 14 (R)→ ∅");
+  testPathTo(tree, 2, "8 (L)→ 3 (L)→ 1 (R)→ ∅");
+
+  // test deleting leaf nodes
+  tree.deleteByValue(13);
+  testPathTo(tree, 13, "8 (R)→ 10 (R)→ 14 (L)→ ∅");
+  
+  // test deleting nodes with single child
+  tree.deleteByValue(10);
+  testPathTo(tree, 10, "8 (R)→ 14 (L)→ ∅");
+  linearTreeRight.deleteByValue(10);
+  testPathTo(linearTreeRight, 15, "8 (R)→ 14 (R)→ 15");
+
+  // test deleting nodes with two children
+  tree.deleteByValue(6);
+  testPathTo(tree, 4, "8 (L)→ 3 (R)→ 7 (L)→ 4");
+  tree.deleteByValue(3);
+  testPathTo(tree, 7, "8 (L)→ 4 (R)→ 7");
+
+  // test deleting root node with two children
+  tree1.deleteByValue(8);
+  testPathTo(tree1, 7, "10 (L)→ 3 (R)→ 6 (R)→ 7");
+  testPathTo(tree1, 13, "10 (R)→ 14 (L)→ 13");
+
+  // test deleting root node with one child
+  linearTreeRight.deleteByValue(8);
+  testPathTo(linearTreeRight, 15, "14 (R)→ 15");
+
+  // test deleting root node with no children
+  singleNodeTree.deleteByValue(8);
+  testPathTo(singleNodeTree, 8, "∅");
 
   std::cout << "All int binary tree tests passed." << std::endl;
 
   Tree<std::string> stringTree = Tree<std::string>("Hello"); stringTree.insert("World");
 
-  stringTree.pathTo("World", pathString); 
-
-  assert(pathString.str() == "Hello (R)→ World");
+  testPathTo(stringTree, std::string("World"), "Hello (R)→ World");
 
   std::cout << "All string binary tree tests passed." << std::endl;
+}
+
+template <typename T>
+void testPathTo(Tree<T> &tree, T value, std::string expectedPath) {
+  std::ostringstream pathString;
+
+  tree.pathTo(value, pathString);
+
+  assert(pathString.str() == expectedPath);
 }
