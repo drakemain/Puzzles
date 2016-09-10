e09794c652ad016e5a034d5ecef631f5867881a4
drakemain <demain1990@gmail.com>
2016-09-09 21:28:11 -0700

    remove extension from binaries

--- c++/build.sh
+++ c++/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-clang++ -std=c++11 -Wall -Wextra -Werror -g ./src/linked-list.cc -o out/linked-list.bin
-clang++ -std=c++11 -Wall -Wextra -Werror -g ./src/binary-tree.cc -o out/binary-tree.bin
-clang++ -std=c++11 -Wall -Wextra -Werror -g ./src/vector.cc -o out/vector.bin
+clang++ -std=c++11 -Wall -Wextra -Werror -g ./src/linked-list.cc -o out/linked-list
+clang++ -std=c++11 -Wall -Wextra -Werror -g ./src/binary-tree.cc -o out/binary-tree
+clang++ -std=c++11 -Wall -Wextra -Werror -g ./src/vector.cc -o out/vector
